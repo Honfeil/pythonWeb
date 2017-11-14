e203b20549c47c33f1e60e35b5512bb3bcb8e9dc
Hash <honfeil@163.com>
2017-11-14 15:49:20 +0800

    flask+Bootstrap 1、url_for接受的参数是views里面所定义的函数 2、mysql数据库爆炸，准备在大型项目设计中再行体会 3、表单需要建造相关实体类，然后通过bootstrap进行渲染

--- test.py
+++ test.py
@@ -1,17 +1,50 @@
-from flask import Flask, request, make_response, redirect, abort, render_template
+import os
+from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_script import Manager, Server
+from flask_moment import Moment
+from datetime import datetime
+from Form import NameForm
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+# 跨站点请求伪造保护
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+# 使用bootstrap
+app.config['BOOTSTRAP_SERVE_LOCAL']=True
+# 使用管理（代码可在控制台运行）
 manager = Manager(app)
 manager.add_command("runserver",Server(host="localhost",port=8888,use_debugger=True))
-app.config['BOOTSTRAP_SERVE_LOCAL']=True
-bootstrap = Bootstrap(app)
+# 使用时间插件（便利而强大的时间功能）
+moment = Moment(app)
+# 设置数据库
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLAlchemy_DATABASE_URI']='mysql:\\\\'+'root:root@localhost:3306/test'
+app.config['SQLALchemy_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+# mysql的url格式
+# mysql://username:password@hostname/database
+
+@app.route('/',methods=['get','post'])
 def index():
-    return "<h1>hello world</h1>"
+    myFrom = NameForm()
+    if myFrom.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name!= myFrom.name.data:
+            # 闪现消息
+            flash('小样，你换了名字我就不认识你了？')
+        session['name']=myFrom.name.data
+        from test import User as uu
+        users = uu.query.all()
+        print(users)
+        return redirect(url_for('index',_external=True))
+    return render_template("index.html",
+                           current_time=datetime.utcnow(),
+                           form = myFrom)
 
 
 @app.route("/user/<name>")
@@ -43,6 +76,31 @@ def redict_to_baidu():
     return redirect("http://www.baidu.com")
 
 
+@app.errorhandler(404)
+def no_page_found(e):
+    return render_template("404.html")
+
+
+@app.errorhandler(500)
+def interl_server_error(e):
+    return render_template("500.html")
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8888,debug=True)
+    db.create_all()
+
+
+class User(db.Model):
+    __tablename__='my_user'
+    user_id = db.Column(db.INTEGER,primary_key=True)
+    user_name = db.Column(db.String,unique=True)
+    user_pwd = db.Column(db.String)
+
+    def __repr__(self):
+        return '<User $r>' % self.user_name
+
+
+
+
+
